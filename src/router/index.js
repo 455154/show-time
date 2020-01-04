f7346900f89e94baa78ee515f2a02ebdea64d21a
cai <1589981380@qq.com>
2020-01-04 09:51:59 +0800

    rencai3

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '../components/Index/index.vue'
+import FullCourse from '../components/FullCourse/FullCourse.vue'
 
 Vue.use(VueRouter)
 const routes = [
@@ -11,6 +12,10 @@ const routes = [
   {
     path: '/index',
     component: Index
+  },
+  {
+    path: '/fullCourse',
+    component: FullCourse
   }
 ]
 
